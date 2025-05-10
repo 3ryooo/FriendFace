584e2eb5dd31127dc2ae9f9676365c90d25d499c
3ryooo <183440614+3ryooo@users.noreply.github.com>
2025-05-10 14:42:24 +0900

    add: UserView

---
+++ FriendFace/View/UserView.swift
@@ -0,0 +1,16 @@
+//
+//  UserView.swift
+//  FriendFace
+//
+
+import SwiftUI
+
+struct UserView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    UserView()
+}
